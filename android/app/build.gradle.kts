75836139a2fba9de8cb719a2e0b21e8deeee13c9
Giribabupeddanagolla <giribabupeddanagolla@gmail.com>
2026-09-17 14:35:54 +0530

    start

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     // Networking & JSON
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
