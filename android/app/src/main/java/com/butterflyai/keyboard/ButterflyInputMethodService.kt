f8423a3ccec2ee67c454b5f88f07b41814072277
Giribabupeddanagolla <giribabupeddanagolla@gmail.com>
2026-09-18 11:09:43 +0530

    yes

--- android/app/src/main/java/com/butterflyai/keyboard/ButterflyInputMethodService.kt
+++ android/app/src/main/java/com/butterflyai/keyboard/ButterflyInputMethodService.kt
@@ -14,6 +14,7 @@ import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.KeyEvent
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
